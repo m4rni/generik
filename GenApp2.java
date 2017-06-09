887e58e86be176b46be5f22003b8c7712f898535
m4rni <marnisu66@gmail.com>
2017-06-09 20:19:20 +0700

    get - setter

---
+++ GenApp2.java
@@ -0,0 +1,30 @@
+
+/**
+ *
+ * @author user
+ */
+public class GenApp2 <T>{
+    private T type;
+
+    public T getType(){
+        return type;
+    }
+   
+    public void setType (T type){
+        this.type = type;
+    }
+   
+    
+    
+    private static <N> void
+            anythingYouWanted(N freeParType){
+                System.out.println(freeParType);
+            }
+           
+            
+            public static void main(String[]args){
+                anythingYouWanted(1);
+               
+                
+            }
+}
